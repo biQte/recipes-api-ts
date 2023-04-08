9922f5e6347f65c3de94db0a2762d05ccfa92d7d
biQte <patrykgonet@gmail.com>
2023-04-08 13:04:46 +0200

    Register new entities

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,19 +1,22 @@
-import { DataSource } from 'typeorm';
-import 'dotenv/config';
-import { Ingredient } from './entity/ingredient';
-import { Recipe } from './entity/recipe';
-import { RecipeIngredient } from './entity/recipe_ingredient';
+import { DataSource } from "typeorm";
+import "dotenv/config";
+import { Ingredient } from "./entity/ingredient";
+import { Recipe } from "./entity/recipe";
+import { RecipeIngredient } from "./entity/recipe_ingredient";
+import { User } from "./entity/user";
+import { Password } from "./entity/password";
+import { Session } from "./entity/session";
 
 export const AppDataSource = new DataSource({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: 5432,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: false,
-    entities: [Ingredient, Recipe, RecipeIngredient],
-    migrations: [],
-    subscribers: [],
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [Ingredient, Recipe, RecipeIngredient, User, Password, Session],
+  migrations: [],
+  subscribers: [],
 });
