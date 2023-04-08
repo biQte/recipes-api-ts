135dfedbb5ee26477ed4a3eb3ab152383b84c93d
biQte <patrykgonet@gmail.com>
2023-04-08 18:12:18 +0200

    Register user route

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 const router = Router();
 
-import recipesList from './recipes';
-import ingredientsList from './ingredients';
+import recipesList from "./recipes";
+import ingredientsList from "./ingredients";
+import Users from "./users";
 
-router.use('/recipes', recipesList);
-router.use('/ingredients', ingredientsList);
+router.use("/recipes", recipesList);
+router.use("/ingredients", ingredientsList);
+router.use("/user", Users);
 
 export default router;
