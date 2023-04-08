9fcc4bebf5c9a1b24d936aa8849dedd807e96f63
biQte <patrykgonet@gmail.com>
2023-04-08 13:05:36 +0200

    Add static images senging

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import express, { json } from 'express';
-import { AppDataSource } from './data-source';
-import apiRouter from './routes/router';
+import express, { json } from "express";
+import { AppDataSource } from "./data-source";
+import apiRouter from "./routes/router";
 
-AppDataSource.initialize().then(async () => {
+AppDataSource.initialize()
+  .then(async () => {
     const { PORT } = process.env;
     const app = express();
 
     app.use(json());
 
-    app.use('/api', apiRouter);
+    app.use("/api", apiRouter);
+    app.use(
+      "/images",
+      express.static("/Users/patryk/Desktop/recipe-app/images")
+    );
 
-    app.listen(PORT, () => {
-        console.log('Hello!');
-    }).on('error', (error) => {
+    app
+      .listen(PORT, () => {
+        console.log("Hello!");
+      })
+      .on("error", (error) => {
         console.error(error);
-    });
-}).catch((error) => console.log(error));
+      });
+  })
+  .catch((error) => console.log(error));
