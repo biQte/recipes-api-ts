38144eaac8e97312c764728dd3dfc7088012ef4b
biQte <patrykgonet@gmail.com>
2023-04-11 15:37:01 +0200

    Add swagger docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { json } from "express";
 import { AppDataSource } from "./data-source";
 import apiRouter from "./routes/router";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "./swagger.json";
 
 AppDataSource.initialize()
   .then(async () => {
@@ -8,8 +10,8 @@ AppDataSource.initialize()
     const app = express();
 
     app.use(json());
-
     app.use("/api", apiRouter);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     app.use(
       "/images",
       express.static("/Users/patryk/Desktop/recipe-app/images")
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,3 +1,38 @@
+/**
+ * @swagger
+ * /example:
+ *      post:
+ *          summary: Send the text to the server
+ *          tags:
+ *              - ExampleEndpoints
+ *          description: Send a message to the server and get a response added to the original text.
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              responseText:
+ *                                  type: string
+ *                                  example: This is some example string! This is an endpoint
+ *          responses:
+ *              201:
+ *                  description: Success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              type: object
+ *                              properties:
+ *                                  text:
+ *                                      type: string
+ *                                      example: This is some example string!
+ *              404:
+ *                  description: Not found
+ *              500:
+ *                  description: Internal server error
+ */
+
 import { Router } from "express";
 import { userController } from "./../controllers/user.controller";
 
